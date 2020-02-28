65c3996f8a76159f19bfca26f3d23f61813da3ef
ehddnr301 <dy950328@gmail.com>
2020-02-28 11:49:14 +0900

    #1 성공시 test

--- index.spec.js
+++ index.spec.js
@@ -1,13 +1,26 @@
-const app = require("./index");
 const request = require("supertest");
+const should = require("should");
+const app = require("./index");
 
 describe("GET /users는", () => {
-  it("user임", done => {
-    request(app)
-      .get("/users")
-      .end((err, res) => {
-        console.log(res.body);
-        done();
-      });
+  describe("성공시", () => {
+    it("유저 객체를 담은 배열로 응답한다.", done => {
+      request(app)
+        .get("/users")
+        .end((err, res) => {
+          res.body.should.be.instanceOf(Array);
+          // 배열임을 검증하는 코드
+          done();
+        });
+    });
+    it("최대 limit 갯수만큼 응답한다", done => {
+      request(app)
+        .get("/users?limit=2")
+        .end((err, res) => {
+          res.body.should.have.lengthOf(2);
+          //   배열 length 가 2임을 검증
+          done();
+        });
+    });
   });
 });
